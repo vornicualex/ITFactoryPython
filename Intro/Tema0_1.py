0ffc42cf6aa2df325027040582ae8ff3049e03ca
vornicualex <60367402+vornicualex@users.noreply.github.com>
2022-09-28 11:16:13 +0300

    Puse in root Intro pentru a fi vazute si in GitH

--- Intro/Tema0_1.py
+++
@@ -1,4 +0,0 @@
-''''
-bool - boolean = sintaxa care poate fi adevarata sau falsa (True or False)
-In cazul Python True/False se va scrie cu litera mare. in Java se scrie cu litera mica
-'''
--- Intro/Tema0_2.py
+++
@@ -1,6 +0,0 @@
-# Variabila String este un sir de caractere. In cazul Python nu este necesara punerea String in fata variabilei pentru ca Python este mai permisiv
-# Sirul de caractere este pus intre doua ghilimele simple (' ')
-# Exemplu: exemplu = exemplu
-
-exemplu = 'exemplu'
-exemplu2 = '1234'
