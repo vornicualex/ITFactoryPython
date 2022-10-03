bd677cfe57b08e90468f7bfdceb8a584d154b84b
vornicualex <60367402+vornicualex@users.noreply.github.com>
2022-10-03 16:29:16 +0300

    Update teme. AM pus toate cerintele astfel incat sa se vada in Git

--- Tema1/radical.py
+++ Tema1/radical.py
@@ -1,11 +1,6 @@
 # Cerinta:
 # Fiind dat un numar citit de la tastatura, sa se afiseze radicalul lui.
 
-# numar_citit = int(input("Introduceti o valoare: "))
-# #print(type(numar_citit))
-# print('Radical din valoarea indrousa este:')
-# print(numar_citit**(1/2))
-
 numar_citit = input("Introduceti o valoare: ")
 numar = int(numar_citit)
 
