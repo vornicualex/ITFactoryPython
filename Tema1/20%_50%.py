bbebd979fd8e5d261ebf7af1243f05c592d0cf71
vornicualex <60367402+vornicualex@users.noreply.github.com>
2022-10-03 16:13:52 +0300

    Update teme. AM pus toate cerintele astfel incat sa se vada in Git

--- Tema1/20%_50%.py
+++ Tema1/20%_50%.py
@@ -7,5 +7,5 @@ jumatate = a*(1/2)
 # print('25% din', a, '=', patrime)    --------- Intelesesm initial ca sa afisat 25% si 25% din intreg. Am lasat totusi si aceste idei
 # print('50% din', a, '=',jumatate)
 print('25% din 50% din', a, '=',jumatate*1/4)
-print(1/4*50)
+
 
